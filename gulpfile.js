f786401a45c0d9e21b59c8478aa5f3339a169b80
Jiri Hybek <jiri.hybek@cryonix.cz>
2015-03-24 02:05:03 +0100

    Major update
    Minor bugfixes and improvements
    Templating engine update
    Fragment lifecyce added (resume / pause)
    Activites update
    Utilities added
    Channel component added
    Gulp build update
    Minified versions of individual components added

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,100 @@ var gulp   = require('gulp'),
 
 var licenseHeader = "/*\n * MetaJS - MetaPlatform project\n *\n * @author Jiri Hybek <jiri.hybek@cryonix.cz> / Cryonix Innovations <www.cryonix.cz>\n * @license MIT\n */\n";
 
+var compress = function(name, src, output){
+
+	return src.pipe(concat(output))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+		.pipe(insert.prepend(licenseHeader))
+		.pipe(gulp.dest('dist/'))
+		.pipe(notify({ message: 'Build complete: ' + name }));
+
+};
+
 gulp.task('default', function() {
-	gulp.start('lib');
+	gulp.start('utils');
+	gulp.start('template');
+	gulp.start('view');
+	gulp.start('fragment');
+	gulp.start('activity');
+	gulp.start('providers');
+	gulp.start('channel');
+	gulp.start('all');
 });
 
-gulp.task('lib', function() {
+gulp.task('utils', function() {
+	
+	var src = gulp.src('lib/utils.js');
+	return compress("Utils", src, "utils.js");
+
+});
+
+gulp.task('template', function() {
+	
+	var src = gulp.src([
+		'lib/utils.js',
+		'lib/template.js'
+	]);
+
+	return compress("Template", src, "template.js");
+
+});
+
+gulp.task('view', function() {
+	
+	var src = gulp.src([
+		'lib/utils.js',
+		'lib/template.js',
+		'lib/view.js'
+	]);
+	
+	return compress("View", src, "view.js");
+
+});
+
+gulp.task('fragment', function() {
+	
+	var src = gulp.src([
+		'lib/utils.js',
+		'lib/template.js',
+		'lib/view.js',
+		'lib/fragment.js'
+	]);
+	
+	return compress("Fragment", src, "fragment.js");
+
+});
+
+gulp.task('activity', function() {
+	
+	var src = gulp.src([
+		'lib/utils.js',
+		'lib/template.js',
+		'lib/view.js',
+		'lib/fragment.js',
+		'lib/activity.js'
+	]);
+	
+	return compress("Activity", src, "activity.js");
+
+});
+
+gulp.task('providers', function() {
+	
+	var src = gulp.src('lib/providers.js');
+	return compress("Providers", src, "providers.js");
+
+});
+
+gulp.task('channel', function() {
+	
+	var src = gulp.src('lib/channel.js');
+	return compress("Channel", src, "channel.js");
+
+});
+
+gulp.task('all', function() {
 	return gulp.src('lib/*.js')
 		.pipe(concat('metajs.js'))
 		.pipe(rename({suffix: '.min'}))
